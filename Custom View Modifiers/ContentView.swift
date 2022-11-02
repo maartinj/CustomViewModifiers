7a2ee088afcd18f0974921edaa9b72d5e841cc48
Marcin Jędrzejak <53284922+maartinj@users.noreply.github.com>
2022-11-02 03:59:45 +0100

    Initial Commit

---
+++ Custom View Modifiers/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  Custom View Modifiers
+//
+//  Created by Stewart Lynch on 2020-12-27.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
